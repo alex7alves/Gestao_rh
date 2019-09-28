76df95ec113460879edf44217e0be37860c98b44
alex7alves <alex7alves10@hotmail.com>
2019-09-28 08:59:04 -0300

    Gerando relacionamento dos modelos

--- apps/documentos/models.py
+++ apps/documentos/models.py
@@ -1,6 +1,10 @@
 from django.db import models
+from apps.funcionarios.models import Funcionario
+
 
 class Documento(models.Model):
     descricao = models.CharField(max_length=200, help_text=' Descricao do documento ')
+    proprietario = models.ForeignKey(Funcionario, on_delete=models.PROTECT)
+
     def __str__(self):
         return self.descricao
--- apps/funcionarios/models.py
+++ apps/funcionarios/models.py
@@ -1,9 +1,13 @@
 from django.db import models
-
-
+from django.contrib.auth.models import User
+from apps.departamentos.models import Departamento
+from apps.empresas.models import empresa
 
 class Funcionario(models.Model):
      nome = models.CharField(max_length=200, help_text='Nome do funcionario')
+     usuario = models.OneToOneField(User, on_delete=models.PROTECT)
+     departamentos = models.ManyToManyField(Departamento)
+     empresa = models.ForeignKey(empresa, on_delete=models.PROTECT)
 
 
      def __str__(self):
