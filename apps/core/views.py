cf71bf87546b672357a63a42bd62253fa0caca5c
alex7alves <alex7alves10@hotmail.com>
2019-10-03 16:33:53 -0300

    Adicionando sistema de login/logout

--- apps/core/views.py
+++ apps/core/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
+# Obrigar se logar para acessar -> vai para pagina de login
+@login_required
 def home(request):
     return render(request,'core/index.html')
--- gestao_rh/urls.py
+++ gestao_rh/urls.py
@@ -20,5 +20,5 @@ urlpatterns = [
     path('', include('apps.core.urls')),
     path('admin/', admin.site.urls),
     path('funcionarios/', include('apps.funcionarios.urls')),
-
+    path('accounts/', include('django.contrib.auth.urls'))
 ]
